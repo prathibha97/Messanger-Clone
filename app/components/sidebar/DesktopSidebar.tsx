38c6563c5cdca5cc36a0d51ca1cb397eb86f1f71
Prathibha Ratnayake <prsthibha@gmail.com>
2023-08-01 11:12:51 +0530

    Add settingsModal and update user function

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -2,8 +2,9 @@
 import useRoutes from '@/app/hooks/useRoutes';
 import { User } from '@prisma/client';
 import { FC, useState } from 'react';
-import DesktopItem from './DesktopItem';
 import Avatar from '../Avatar';
+import DesktopItem from './DesktopItem';
+import SettingsModal from './SettingsModal';
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -14,6 +15,8 @@ const DesktopSidebar: FC<DesktopSidebarProps> = ({ currentUser }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
+    <>
+    <SettingsModal currentUser={currentUser} isOpen={isOpen} onClose={()=>setIsOpen(false)}/>
     <div className='hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between'>
       <nav className='mt-4 flex flex-col justify-between'>
         <ul role='list' className='flex flex-col items-center space-y-1'>
@@ -34,10 +37,11 @@ const DesktopSidebar: FC<DesktopSidebarProps> = ({ currentUser }) => {
           onClick={() => setIsOpen(true)}
           className='cursor-pointer hover:opacity-75 transition'
         >
-          <Avatar user={currentUser}/>
+          <Avatar user={currentUser} />
         </div>
       </nav>
     </div>
+    </>
   );
 };
 
